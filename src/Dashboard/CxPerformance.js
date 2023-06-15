f66c5423a88df16e874e728d9676e019a8255846
Ashritha K A <ashritha.ka@factspan.com>
2023-06-15 14:59:57 +0530

    Committed new files

--- src/Dashboard/CxPerformance.js
+++ src/Dashboard/CxPerformance.js
@@ -1,81 +1,80 @@
-import React from 'react'
-import { Doughnut } from 'react-chartjs-2';
-// import { Chart, registerables } from 'chart.js';
+import React from "react";
+import "./CxPerformance.css";
+import { BsArrowLeft } from "react-icons/bs";
+import { Link } from "react-router-dom";
+import ChartCxPerformance from "../Chart/ChartCxPerformance";
+import ChartMulti from "../Chart/ChartMulti";
+import ChartVertBar from "../Chart/ChartVertBar";
+import Map from "../Chart/Map";
 
-
-
-export const data = {
-    labels :['Loss', 'Won', 'In Progress'],
-    datasets: [
-      {
-        label: ['Loss', 'Won', 'In Progress'],
-        data: [262, 680, 634],
-        backgroundColor: [
-            'rgb(232,209,102)',
-            'rgb(230,143,150)',
-            'rgb(77,213,255)'
-
-        ],
-        borderColor: [
-            'rgb(232,209,102)',
-            'rgb(230,143,150)',
-            'rgb(77,213,255)'
-        ],
-        borderWidth: 1,
-      },
-    ],
-};
-
-// const options = {
-//     plugins: {
-//       legend: {
-//         display: "false", // Hide the legend
-//       },
-//       labels: {
-//         render: 'labels', // Display labels
-//         position: 'bottom', // Position labels outside the doughnut chart
-//         arc: true, // Show labels for each slice of the doughnut
-//         font: {
-//           weight: 'bold', // Make labels bold
-//         },
-//       },
-//     },
-//     scales: {
-//       grid: {
-//         display: false, // Remove grid lines
-//       },
-//     },
-// };
-
-// const labels =['Loss', 'Won', 'In Progress'];
-
-const ChartCxPerformance = () => {
-    
+const CxPerformance = () => {
   return (
-    <div style={{ display: 'flex' }}>
-      <div style={{ display: 'flex', flexDirection: 'column-reverse', marginLeft: '10px' }}>
-        {data.labels.map((label, index) => (
-          <div key={index} style={{ display: 'flex',flexDirection:'row', alignItems: 'center', marginBottom: '5px', fontSize:'8px' }}>
-            <span
-              style={{
-                display: 'inline-block',
-                alignItems:'center',
-                width: '12px',
-                height: '12px',
-                backgroundColor: data.datasets[0].backgroundColor[index],
-                borderRadius: '50%',
-                marginRight: '5px',
-                fontSize:'8px'
-              }}
-            ></span>
-            <span>{label}</span>
-          </div>
-          
-        ))}
-        <Doughnut data={data}   />
+    <div className="dashboard-main">
+      <div>
+        <Link  to={'/'} style={{color:"white"}}><BsArrowLeft className="go-back" size={35} /></Link>
+        <div className="header-main">
+          <div className="header-title">CX Performance Overview</div>
+        </div>
+        <hr className="header-line" />
+        <div className="container-fluid">
+            <div className="row">
+                <div className="col-md-2">
+                    <div className="card mb-3">
+                    <p style={{fontSize:28}}>4K</p>
+                    <p style={{color:"#47C8E7"}}>Number of FFEs</p>
+                    </div>
+                    <div className="card mb-2">
+                    <p style={{fontSize:28}}>$3M</p>
+                    <p style={{color:"#47C8E7"}}>Ocean+CX Revenue</p>
+                    </div>
+                    <div className="filter-heading">User Name</div>
+                    <select className="dropdown" id="">
+                        <option  value="">All</option>
+                    </select>
+                    <div className="filter-heading">Product Sold Reason</div>
+                    <select className="dropdown" id="">
+                        <option value="">All</option>
+                    </select>
+                    <div className="filter-heading">Booking Number</div>
+                    <select className="dropdown" id="">
+                        <option value="">All</option>
+                    </select>
+                    <div className="filter-heading">Completion Date</div>
+                    <select className="dropdown" id="">
+                        <option value="">All</option>
+                    </select>
+                    <div className="filter-heading">Product Not Sold Reason</div>
+                    <select className="dropdown" id="">
+                        <option value="">All</option>
+                    </select>
+                    <div className="filter-heading">Reason for In-Progress</div>
+                    <select className="dropdown" id="">
+                        <option value="">All</option>
+                    </select>
+                </div>
+                <div className="col-md-5">
+                    <div className="card mb-3">
+                        <div className="card-head">Deal Status</div>
+                        <div className="card-title">By Booking Numbers</div>
+                        <ChartCxPerformance  className="chart-container" />
+                    </div>
+                    <div className="card mb-2">
+                    <div className="card-head">Revenue and Revenue MoM%</div>
+                        <ChartMulti /></div>
+                </div>
+                <div className="col-md-5">
+                    <div className="crad">
+                        <div className="card mb-3">
+                            <div className="card-head">Revenue by Product</div>
+                            <ChartVertBar /></div>
+                        <div className="card mb-2"><Map /></div>
+                    </div>
+                </div>
+            </div>
+        </div>
       </div>
     </div>
   );
-}
+};
 
-export default ChartCxPerformance
+export default CxPerformance;
